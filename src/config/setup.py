919a30594719c2204e112b539c362bc094131e90
Arun Shankar <arunprshankar@gmail.com>
2024-10-04 16:49:11 -0500

    wip

---
+++ src/config/setup.py
@@ -0,0 +1,66 @@
+from src.config.logging import logger
+from typing import Dict
+from typing import Any
+import yaml
+import os
+
+
+class Config:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Config, cls).__new__(cls)
+            # The following line ensures that the __init__ method is only called once.
+            cls._instance.__initialized = False
+        return cls._instance
+    
+    def __init__(self, config_path: str = "./config/config.yml"):
+        """
+        Initialize the Config class.
+
+        Args:
+        - config_path (str): Path to the YAML configuration file.
+        """
+        if self.__initialized:
+            return
+        self.__initialized = True
+        
+        self.__config = self._load_config(config_path)
+        self.PROJECT_ID = self.__config['project_id']
+        self.GEMINI_REGION = self.__config['gemini_region']
+        self.ANTHROPIC_REGION = self.__config['anthropic_region']
+        self.CREDENTIALS_PATH = self.__config['credentials_json']
+        self._set_google_credentials(self.CREDENTIALS_PATH)
+        self.GEMINI_MODEL_NAME = self.__config['gemini_model']
+        self.ANTHROPIC_MODEL_NAME = self.__config['anthropic_model']
+
+    @staticmethod
+    def _load_config(config_path: str) -> Dict[str, Any]:
+        """
+        Load the YAML configuration from the given path.
+
+        Args:
+        - config_path (str): Path to the YAML configuration file.
+
+        Returns:
+        - dict: Loaded configuration data.
+        """
+        try:
+            with open(config_path, 'r') as file:
+                return yaml.safe_load(file)
+        except Exception as e:
+            logger.error(f"Failed to load the configuration file. Error: {e}")
+
+    @staticmethod
+    def _set_google_credentials(credentials_path: str) -> None:
+        """
+        Set the Google application credentials environment variable.
+
+        Args:
+        - credentials_path (str): Path to the Google credentials file.
+        """
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+
+config = Config()
